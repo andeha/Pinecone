fbb8f77c8ffb3d26beff5a9db64810627caffe1f
Anders Hansson <anders.pm.hansson@gmail.com>
2018-04-25 11:02:58 +0200

    Improved exposition.

--- Additions/SlowRandom.h
+++ Additions/SlowRandom.h
@@ -1,55 +1,54 @@
 //
 //  SlowRandom.h
-//  Pinecone
+//  Pinecone Additions
 //
 
 #ifndef __SLOWRANDOM_H
 #define __SLOWRANDOM_H
 
-#include <PineconeLite/Pinecone.h>
+#include <Pinecone.h>
 
-ENCLAVED
 struct SlowRandom { // 😐🎲 See [Unittests]>--<SyncTests.cpp>{Synchronization_rendezvousbroadcast} for details.
-
+    
     enum class Solution { RAND, SEED };
-
+    
     SlowRandom(uint64_t *valueOptAlsoSeed, Solution s) FALLIBLE;
-
+    
     SlowRandom(Vector<uint64_t> &values, Solution s) FALLIBLE;
-
+    
     ~SlowRandom();
     
     INLINED void update() const BLURTS;
-	
-  	/**  Approximations for normally distributed random variates. */
-
+    
+    /**  Approximations for normally distributed random variates. */
+    
     enum GaussianApproximate { AbramowitzStegun, ZogheibHlynka };
-	
-	/**
- 
-     To transformed into a normally distributed random variable:
+    
+    /**
+    
+     To transform into a normally distributed random variable, calculate:
     
      @code
      double notgaussian = value * stddev + mean
      @endcode
- 
+    
      */
     
     INLINED static double gaussian(GaussianApproximate ga);
-	
-	/**
- 
+    
+    /**
+    
      Return a uniformly distriuted random value ∈ [0-𝜀, 1+𝜀]. To lax the distri-
      bution to @c value ∈ [min, max], compute:
- 
+    
      @code
      double myuniform = value*(max - min) + min
      @endcode
- 
+    
      */
-
+    
     INLINED static double uniform(); // value between 0 and 1
-
+    
     enum class ErrorCode { unableToRAND, unableToSEED };
     
     MACRO static void Error(ErrorCode code) BLURTS {
@@ -59,9 +58,9 @@ struct SlowRandom { // 😐🎲 See [Unittests]>--<SyncTests.cpp>{Synchronizatio
         }
         BLURT("General exception");
     }
-
-😐;
     
+😐;
+
 #define 🎲(sides) { uint64_t value, SlowRandom rnd(&value, SlowRandom::Solution::RAND), rnd.update(), value % sides; }
 
 #endif
