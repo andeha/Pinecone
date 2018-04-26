5099554244b178c567af712e7bb47078239af0cb
Anders Hansson <anders.pm.hansson@gmail.com>
2018-04-26 10:18:16 +0200

    Improved formatting.

--- System/conio.cpp
+++ System/conio.cpp
@@ -6,22 +6,37 @@
 #include <Pinecone.h>
 
 #ifdef __x86_64__
+
 #include <stdio.h>
+
 uint8_t getch() { return (uint8_t)getc(stdin); }
 void putch(uint8_t data) { putchar(data); }
 void initUART() {}
+
 #elif __mips__
-void initUART() {
+
+void
+initUART()
+{
+    // TODO: Fine-tune oscillator
     🔧(PIC32MZ_U5MODESET) = UxMODE_ON; // UART5 is enabled
     🔧(PIC32MZ_U5STASET)  = UxSTA_UTXEN; // UART5 transmitter is enabled
 }
 
-void putch(uint8_t c) {
+void
+putch(
+    uint8_t c
+)
+{
     while (UxSTA_UTXBF & 🔎(PIC32MZ_U5STA)); // Wait while buffer is full
     🔧(PIC32MZ_U5TXREG) = c; // Transmit character
 }
 
-void getch(uint8_t c) {
+uint8_t
+getch(
+    uint8_t c
+)
+{
     if (UxSTA_OERR & 🔎(PIC32MZ_U5STA)) { 🔧(PIC32MZ_U5STACLR) = UxSTA_OERR; }
     while (UxSTA_URXDA & 🔎(PIC32MZ_U5STA) == 0);
     uint8_t result = 🔎(PIC32MZ_U5RXREG);
