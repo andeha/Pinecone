87bbc25ae5b616dce1b87dc689c9a81f018ac1d0
Anders Hansson <anders.pm.hansson@gmail.com>
2018-05-08 14:05:56 +0200

    Added Nucleus header file.

---
+++ PineconeNucleus.h
@@ -0,0 +1,51 @@
+//
+//  PineconeNucleus.h (i.e 🌱.h)
+//  Pinecone
+//
+
+#ifndef __PINECONENUCLEUS_H
+#define __PINECONENUCLEUS_H
+
+// The following headers constitute the Pinecone library operating system
+
+#include <Additions/Performance.h>
+#include <Additions/Helpers.h>
+#include <Additions/Chronology.h>
+#include <Additions/SlowRandom.h>
+#include <Additions/Arithmetics.h>
+#include <Additions/Mathematics.h>
+#include <Additions/Bloomfilter.h>
+#include <Additions/Subgraph.h>
+#include <Additions/Bezier.h>
+#include <Additions/Guid.h>
+#include <Additions/Compression.h>
+#include <Additions/Base64.h>
+#include <Additions/Security.h>
+#include <Additions/Interleave.h>
+#include <Additions/FontRasterizer.h>
+#include <Additions/Parsers.h>
+#include <Additions/Regex.h>
+#include <Additions/ModernIntel.h>
+#include <Additions/Ieee754b10.h>
+#include <Additions/MoreMaths.h>
+// #include <Additions/Utf8.h>
+// #include <Additions/Locales.h>
+// #include <Additions/Filesystem.h>
+// #include <Additions/DynamicLinkLibrary.h>
+// #include <Additions/MachOParser.h>
+// #include <Additions/Enclave.h>
+// #include <Additions/Evaluationboards.h>
+// #include <Additions/FileTerminal.h>
+// #include <Additions/Commandline.h>
+// #include <Additions/Password.h>
+// #include <Additions/TransactionalMemory.h>
+// #include <Additions/VirtualMemory.h>
+// #include <Additions/Plugin.h>
+// #include <Additions/Notification.h>
+// #include <Additions/ConsoleTerminal.h>
+#include <Additions/Task.h>
+#include <Additions/ModernTask.h>
+#include <Additions/Hypervisor.h>
+#include <Additions/Formatting.hpp>
+
+#endif
